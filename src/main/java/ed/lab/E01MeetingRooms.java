6819456e235167626db0a039f21e1d277fd107d8
Gerardo Avila <gjavila@correo.url.edu.gt>
2025-05-23 08:09:39 -0600

    Parcial 02 Me costo entender el segundo problema

--- src/main/java/ed/lab/E01MeetingRooms.java
+++ src/main/java/ed/lab/E01MeetingRooms.java
@@ -1,11 +1,29 @@
 package ed.lab;
 
-import java.util.List;
+import java.util.*;
 
 public class E01MeetingRooms {
 
     public int minMeetingRooms(List<MeetingInterval> meetingIntervals) {
-        return -1;
-    }
+        if (meetingIntervals == null || meetingIntervals.isEmpty()) {
+            return 0;
+        }
+        //Primero se ordena
+        meetingIntervals.sort(Comparator.comparingInt(a -> a.startTime()));
 
+        PriorityQueue<Integer> heap = new PriorityQueue<>();
+        heap.offer(meetingIntervals.get(0).endTime());
+
+        //para cuando i sea mayor que meeting interval.size
+        int maxRooms = 1;
+        for (int i = 1; i < meetingIntervals.size(); i++) {
+            MeetingInterval current = meetingIntervals.get(i);
+            if (current.startTime() >= heap.peek()) {
+                heap.poll();
+            }
+            heap.offer(current.endTime());
+            maxRooms = Math.max(maxRooms, heap.size());
+        }
+        return maxRooms;
+    }
 }
--- src/main/java/ed/lab/E02Logger.java
+++ src/main/java/ed/lab/E02Logger.java
@@ -1,12 +1,27 @@
 package ed.lab;
 
+import java.util.HashMap;
+
 public class E02Logger {
 
+    private HashMap<String, Integer> messageMap;
+
     public E02Logger() {
+        messageMap = new HashMap<>();
     }
 
     public boolean shouldPrintMessage(int timestamp, String message) {
-        return false;
+        if (!messageMap.containsKey(message)) {
+            messageMap.put(message, timestamp);
+            return true;
+        } else {
+            int lastTimestamp = messageMap.get(message);
+            if (timestamp >= lastTimestamp + 10) {
+                messageMap.put(message, timestamp);
+                return true;
+            } else {
+                return false;
+            }
+        }
     }
-
 }
